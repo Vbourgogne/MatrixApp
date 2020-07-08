3e38e8bcf3cfd71cba578e7e7439084f95c2b22f
Valentin Bourgogne <valentin.bourgogne@lilo.org>
2020-07-08 23:25:02 +0200

    Aikido reste couleur et messages

--- Assets/Scripts/AikidoManager.cs
+++ Assets/Scripts/AikidoManager.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class AikidoManager : MonoBehaviour
 {
+    public GameObject[] AikidoScreens;
+    public int indexScreens = 0;
+    public GameObject currentScreen;
+    public UIManager UIScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (GameObject screen in AikidoScreens)
+        {
+            screen.SetActive(false);
+        }
+        currentScreen = AikidoScreens[indexScreens];
+        currentScreen.SetActive(true);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        Debug.Log("oui");
+        ScreenChange();
+    }
+    // Update is called once per frame
+    void ScreenChange()
+    {
+        if (indexScreens < AikidoScreens.Length - 1)
+        {
+            currentScreen.SetActive(false);
+            indexScreens++;
+            AikidoScreens[indexScreens].SetActive(true);
+            currentScreen = AikidoScreens[indexScreens];
+        }
+        else
+        {
+
+        }
     }
 }
