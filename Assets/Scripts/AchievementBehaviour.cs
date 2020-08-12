22a80efef096c720b1e15715e645355cf98a2ced
Unknown <awned@hotmail.fr>
2020-08-12 23:33:47 +0200

    Notification Achievements

---
+++ Assets/Scripts/AchievementBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AchievementBehaviour : MonoBehaviour
+{
+    public List <int> achievementsUnlocked;
+    public Animator achievementNotificationAnimator;
+    public Image achievementNotificationImage;
+    public TextMeshProUGUI achievementNotificationText;
+    public Sprite[] achievementImages;
+    public string[] achievementTexts;
+
+    public void AchievementCheck(float variable, float condition, int indexAchievement)
+    {
+        if (variable >= condition)
+        {
+            achievementNotificationImage.sprite = achievementImages[indexAchievement];
+            achievementNotificationText.text = achievementTexts[indexAchievement];
+            achievementNotificationAnimator.SetTrigger("AchievementGet");
+
+            achievementsUnlocked.Add(indexAchievement);
+        }
+    }
+}
--- Assets/Scripts/AchievementScreenBehaviour.cs
+++ Assets/Scripts/AchievementScreenBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class AchievementObjectBehavior : MonoBehaviour
+public class AchievementScreenBehaviour : MonoBehaviour
 {
     public bool isAchieved;
     public float condition;
